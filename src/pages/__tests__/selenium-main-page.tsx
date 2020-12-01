960a4b7d08f8c04a1674c83549fe67bf0550d0c2
Jeremy Marino <jermarino16@gmail.com>
2020-11-30 19:46:59 -0800

    email input works properly

--- src/pages/__tests__/selenium-main-page.tsx
+++ src/pages/__tests__/selenium-main-page.tsx
@@ -1,6 +1,6 @@
 import { cleanup } from '../../test-utils';
 
-const {Builder, By, Key, until, WebElement} = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 const assert = require("assert");
 const sleep = require("sleep");
 
@@ -9,45 +9,57 @@ describe('Main Page', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
-  it('Able to access the main page', async () => {
-
-        let driver = await new Builder().forBrowser('firefox').build();
-        try {
-          await driver.get('http://localhost:3000');
-          let url = await driver.getCurrentUrl();
-          assert.equal(url, "http://localhost:3000/", "Able to access main page");
-  
-          // sleep.sleep(10) // sleep for n seconds
-        } finally {
-          await driver.quit();
-        }
-        
-  }, 30000); //timeout after 30seconds
-
   it('Main page renders properly', async () => {
 
         let driver = await new Builder().forBrowser('firefox').build();
         try {
           //Go to the main page
           await driver.get('http://localhost:3000');
+          let url = await driver.getCurrentUrl();
+          assert.equal(url, "http://localhost:3000/", "Able to access main page");
 
           //Verify there is a title
           let title = await driver.findElement(By.className("css-1r0stgl"));
           title = await title.getText();
           assert.equal(title, "Space Explorer", "The main page has a title");
 
-          //Get the input Box, should be empty
-          let input = await driver.findElement(By.name("email"));
-          let inputText = await input.getText();
-          assert.equal(inputText, "", "Input is on the page and empty");
+        } finally {
+          await driver.quit();
+        }
+        
+  }, 30000); //timeout after 30seconds
+
+  it('Main page email input works properly', async () => {
+
+        let driver = await new Builder().forBrowser('firefox').build();
+        try {
+          //Go to the main page
+          await driver.get('http://localhost:3000');
+
+          //Get the email field, should be empty
+          let emailField = await driver.findElement(By.name("email"));
+          let emailText = await emailField.getText();
+          assert.equal(emailText, "", "Email field is on the page and empty");
 
           //Send keys to the input
-          await input.sendKeys("Test@test.com");
-          inputText = await input.getAttribute("value");
-          assert.equal(inputText, "Test@test.com", "Email Input is able to receive keyboard input");
+          await emailField.sendKeys("Test@test.com");
+          emailText = await emailField.getAttribute("value");
+          assert.equal(emailText, "Test@test.com", "Email field is able to receive keyboard input");
 
-        //   await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-        //   await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+          //remove n keys from input
+          let n = 3;
+          for (let i=0; i<n; i++){
+            await emailField.sendKeys(Key.BACK_SPACE);
+          }
+          emailText = await emailField.getAttribute("value");
+          assert.equal(emailText, "Test@test.", "Email field is able to receive keyboard delete");
+
+          //remove all text from input
+          await emailField.sendKeys(Key.chord(Key.CONTROL, "a"));
+          await emailField.sendKeys(Key.BACK_SPACE);
+          emailText = await emailField.getAttribute("value");
+          assert.equal(emailText, "", "Email field is able to be cleared");
+
         } finally {
           await driver.quit();
         }
